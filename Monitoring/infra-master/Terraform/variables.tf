9ed4cc04124c8efe120b8bc4ceeedee7de807290
Lucifxr6x6 <103212710+Lucifxr6x6@users.noreply.github.com>
2025-04-02 20:52:58 +0300

    Add files via upload

---
+++ Monitoring/infra-master/Terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "folder_id" {
+  description = "The ID of the Yandex Cloud folder"
+  type        = string
+  value       = "b1g1e9i2k7p6fev5fp45"
+}
+
+variable "token" {
+  description = "Token"
+  type        = string
+  value       = "y0_AgAAAAB55grFAATuwQAAAAEXTicEAAD0YcYvULhL05z2WRikKD2zAF0e3g"
+}
+
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+  value       = "b1gcsikpotf0sukdq4r6"
+}
